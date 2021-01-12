1c308a4110c3fcafdb28babf396c55f75203fda9
Guillaume Gouy <guillaumegouy@sfr.fr>
2021-01-12 10:43:45 +0100

    eureka sans currencyexchange

---
+++ microservice-eureka/src/test/java/com/tactfactory/microservice/eureka/MicroserviceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
