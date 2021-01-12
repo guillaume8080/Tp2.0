20bf141fa102d40f2b03001bfe595b3720051760
Guillaume Gouy <guillaumegouy@sfr.fr>
2021-01-12 11:12:27 +0100

    moteurConversion

---
+++ microservice-currencyconversionservice/src/test/java/com/tactfactory/microservice/currencyconversionservice/MicroserviceCurrencyconversionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactfactory.microservice.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCurrencyconversionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
